853b6ca7f14b0c7476d7d3ec92a44754ebb91379
Ahmed Tarek <dark_cat_95@yahoo.com>
2025-11-21 23:05:30 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ const formatBasePath = (basePath: string) => {
   return basePath;
 };
 
-const DEFAULT_REPO_BASE = "msketch";
+const DEFAULT_REPO_BASE = "me";
 
 const resolveBasePath = () => {
   if (process.env.VITE_BASE_PATH) {
