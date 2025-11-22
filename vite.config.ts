b5ddc9c62f8f48cd5e55e4eda772590ea7208186
msketch93 <nono.medo281512@gmail.com>
2025-11-22 11:09:24 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 const formatBasePath = (basePath: string) => {
@@ -42,9 +42,7 @@ export default defineConfig(({ mode }) => {
     },
     base: isDev ? "/" : resolveBasePath(),
     plugins: [
-      react({
-        fastRefresh: false,
-      }),
+      react(),
     ].filter(Boolean),
     resolve: {
       alias: {
